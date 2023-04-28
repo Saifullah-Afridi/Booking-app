69e5e671994da759e0ba80e7e2a00f1c982f3f4f
Saifullah-Afridi <saifullahafridi08@gmail.com>
2023-04-28 12:10:20 +0500

    commit

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
-require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+const morgan = require("morgan");
 const port = process.env.PORT || 5000;
+const mongoose = require("mongoose");
 
+//by making it true only the field specified in the schema will store in databasee
+const connect = async () => {
+  try {
+    mongoose.set("strictQuery", true);
+    await mongoose.connect(process.env.DB);
+    console.log("connected to database");
+  } catch (error) {
+    // console.log(error);
+    console.log("does not connected to database");
+  }
+};
 // !important!
 // you need to install the following libraries |express|[dotenv > if required]
 // or run this command >> npm i express dotenv
@@ -17,6 +31,7 @@ app.get("/", (req, res) => {
   res.send("hello from simple server :)");
 });
 
-app.listen(port, () =>
-  console.log("Server is up and running on port : " + port)
-);
+app.listen(port, () => {
+  connect();
+  console.log("Server is up and running on port : " + port);
+});
