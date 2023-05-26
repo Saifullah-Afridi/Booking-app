02b821197cd64193fa6d3ffc201168f5e610ddf2
Saifullah-Afridi <saifullahafridi08@gmail.com>
2023-05-27 00:56:46 +0500

    Frontend

--- Frontend/src/Pages/hotel/Hotel.js
+++ Frontend/src/Pages/hotel/Hotel.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "./Hotel.css";
+import Navbar from "../../components/Navbar";
+import Header from "../../components/Header";
 
 const Hotel = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <Header type="list" />
+      <div className="hotel-container">
+        <div className="hotel-wrapper">
+          <h1 className="hotel-title">Grand hotel</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hotel;
